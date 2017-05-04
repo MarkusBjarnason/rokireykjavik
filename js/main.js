a46926981c55a345728df4dac4b8e915a12c6253
Jónína Ósk Hansen <Jnna sk Hansen>
2017-05-04 10:08:52 +0000

    vinna í mainsite, þarf að staðsetja takka betur- og gera skalanlega

--- js/main.js
+++ js/main.js
@@ -171,10 +171,80 @@ window.onload = function (event) {
 
 
 
+var knob = $('.knob');
+var angle = 70;
+var minangle = 70;
+var maxangle = 290;
 
+function moveKnob(direction) {
+  
+  if(direction == 'up') {
+    if((angle + 2) <= maxangle) {
+      angle = angle + 2;
+      setAngle();
+    }
+  }
+  
+  else if(direction == 'down') {
+    if((angle - 2) >= minangle) {
+      angle = angle - 2;
+      setAngle();
+    }
+  }
 
+};
+
+function setAngle() {
+
+  // rotate knob
+  knob.css({
+    '-moz-transform':'rotate('+angle+'deg)',
+    '-webkit-transform':'rotate('+angle+'deg)',
+    '-o-transform':'rotate('+angle+'deg)',
+    '-ms-transform':'rotate('+angle+'deg)',
+    'transform':'rotate('+angle+'deg)'
+  });
+};
+  // highlight ticks
+//   var activeTicks = (Math.round(angle / 10) + 1);
+//   $('.tick').removeClass('activetick');
+//   $('.tick').slice(0,activeTicks).addClass('activetick');
+  
+//   // update % value in text
+//   var pc = Math.round((angle/270)*100);
+//   $('.current-value').text(pc+'%');
+  
+// }
+
+// mousewheel event - firefox
+knob.bind('DOMMouseScroll', function(e){
+  if(e.originalEvent.detail > 0) {
+    moveKnob('down');
+  } else {
+    moveKnob('up');
+  }
+  return false;
+});
+
+// mousewheel event - ie, safari, opera
+knob.bind('mousewheel', function(e){
+  if(e.originalEvent.wheelDelta < 0) {
+    moveKnob('down');
+  } else {
+    moveKnob('up');
+  }
+  return false;
+});
 
   
+//kóði til þess draga takkana 
+Draggable.create(".knob", {
+  type: "rotation",
+  throwProps: true,
+  onDrag: function() {
+    console.log(this.rotation)
+  }
+});
 
 
 
